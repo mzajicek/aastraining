a6a97628c3fc25ee748c2a3a52faea167746a027
mzajicek <mart.zajicek@gmail.com>
2019-07-29 14:52:36 +0200

    refactoring -> code to methods

--- src/test/java/tests/CalculationTest.java
+++ src/test/java/tests/CalculationTest.java
@@ -59,24 +59,40 @@ public class CalculationTest {
         Assert.assertFalse(driver.findElement(By.cssSelector("div.result > div:nth-child(3) > p")).getText().isEmpty());
     }
 
+    @Test
+    public void itShouldCalculateIncomeForEachFund(){
+        String[] arrayOfFunds={"Batman's Cave Development","Handelsbanken Aktiv 100","Hoggwart's Fund"};
+        for (String arrayOfFund : arrayOfFunds) {
+            selectFund(arrayOfFund);
+            selectInvestment("1000");
+            selectYears("5");
+            selectEmail("test@test.sk");
+            Assert.assertFalse(driver.findElement(By.cssSelector("div.result > div:nth-child(1) > p")).getText().isEmpty());
+            Assert.assertTrue(driver.findElement(By.cssSelector("div.result > div:nth-child(1) > p")).getText().contains("kr"));
+        }
+    }
+
     private void selectFund(String fundToSelect){
         new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText(fundToSelect);
     }
 
     private void selectInvestment(String investmentToSelect){
+        driver.findElement(By.id("oneTimeInvestmentInput")).clear();
         driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(investmentToSelect);
     }
     private void selectYears(String yearsToSelect){
+        driver.findElement(By.id("yearsInput")).clear();
         driver.findElement(By.id("yearsInput")).sendKeys(yearsToSelect);
     }
     private void selectEmail(String emailToSelect){
+        driver.findElement(By.id("emailInput")).clear();
         driver.findElement(By.id("emailInput")).sendKeys(emailToSelect);
     }
 
 
     @After
     public void tearDown() {
-        driver.close();
-        driver.quit();
+//        driver.close();
+//        driver.quit();
     }
 }
