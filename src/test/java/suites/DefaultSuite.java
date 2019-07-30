119684ef76c2353f46e48fd4f8a23e589004274c
mzajicek <mart.zajicek@gmail.com>
2019-07-30 16:03:47 +0200

    final version as of 30.7.2019

--- src/test/java/suites/DefaultSuite.java
+++ src/test/java/suites/DefaultSuite.java
@@ -3,13 +3,17 @@ package suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import tests.CalculationTest;
+import tests.EmailValidationTest;
+import tests.NewSavingRequestTest;
 import tests.SavingCalculatorTest;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         CalculationTest.class,
-        SavingCalculatorTest.class
+        SavingCalculatorTest.class,
+        EmailValidationTest.class,
+        NewSavingRequestTest.class
 })
 public class DefaultSuite {
 
--- src/test/java/tests/EmailValidationTest.java
+++ src/test/java/tests/EmailValidationTest.java
@@ -6,12 +6,38 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import pages.CalculatorPage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmailValidationTest extends TestBase {
     @Test
-    public void itShouldDisplayErrorWhenEmailIsInvalid() {
+    public void itShouldDisplayErrorWhenEmailIsInvalid() throws FileNotFoundException {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        calculatorPage.selectEmail("skdffdjs");
+        //calculatorPage.selectEmail("skdffdjs");
+        for (String readInvalidEmail : readInvalidEmails()){
+        calculatorPage.selectEmail(readInvalidEmail);
+            Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='emailInput']]")).getAttribute("class").contains("error"));
+        }
 
-        Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='emailInput']]")).getAttribute("class").contains("error"));
+
     }
+
+    private List<String> readInvalidEmails() throws FileNotFoundException {
+        //nacitam file
+        FileReader invalidEmailsTxt = new FileReader(new File("src/test/resources/invalid_emails.txt"));
+        //vytvorim prazdny list do ktoreho budem ukladat riadky
+        List<String> invalidEmailList = new ArrayList<>();
+        //prechadzam cez riadky textaku a zozbieram ich do listu
+        //do vytvoreneho listu priradim vsetky riadky ktore som pridal
+        invalidEmailList = new BufferedReader(invalidEmailsTxt).lines().collect(Collectors.toList());
+        //vratim list
+        return invalidEmailList;
+    }
+
+
 }
